9f54424f96d2523dfba94bef35210a7365eb4f8d
DpalmeITESM <a01747290@itesm.mx>
2020-01-05 06:25:17 -0600

    verguísima we

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,6 @@
 import pyperclip
 
-lines = open("poem.txt", "r").readlines()
+lines = open("poem.txt", "r", encoding="UTF-8").readlines()
 
 final_string = '"' + lines[0].strip().replace('"', "'") + '", "'
 for line in lines[1:]:
