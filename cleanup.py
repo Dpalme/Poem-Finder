30b46285815ab86cdd441065ae69416b46a5d7f4
DpalmeITESM <a01747290@itesm.mx>
2020-01-05 06:05:43 -0600

    fuckton of poems

---
+++ cleanup.py
@@ -0,0 +1,9 @@
+import pyperclip
+
+lines = open("poem.txt", "r").readlines()
+
+final_string = '"' + lines[0].strip().replace('"', "'") + '", "'
+for line in lines[1:]:
+    final_string += line.strip().replace('"', "'") + "\\" + "n"
+final_string += '"'
+pyperclip.copy(final_string)
