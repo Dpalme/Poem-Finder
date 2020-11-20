01739676436cdb8060f255c47ea8ee9891a94810
Dpalme <diegoPalmerin24@gmail.com>
2020-11-19 18:28:55 -0600

    colors

---
+++ js/color.js
@@ -0,0 +1,26 @@
+class Color {
+    constructor (value) {
+        new RandomObject("color", "", "#" + value, "", "./color/?c=" + value);
+    }
+}
+
+new Color("EFE6DF");
+new Color("FFAA1B");
+new Color("F4254F");
+new Color("5056EC");
+new Color("40427F");
+new Color("FF1D6F");
+new Color("4B61C6");
+new Color("FEBA29");
+new Color("17181C");
+new Color("F4442D");
+new Color("F2F3AF");
+new Color("00326D");
+new Color("BC937D");
+new Color("7AC6BE");
+new Color("00847B");
+new Color("94C7AE");
+new Color("CA8D85");
+new Color("EE3C5E");
+new Color("F7924C");
+new Color("464996");
--- poem_base_builder.py
+++ poem_base_builder.py
@@ -1,4 +1,8 @@
-with open("js/shows.js", "r+", encoding="UTF-8") as archivo:
-    lines = open("input.txt", "r", encoding="UTF-8").readlines()
-    for line in lines:
-        archivo.write('new Show("%s");\n' % line.strip())
+import pyperclip
+
+lines = open("input.txt", "r", encoding="UTF-8").readlines()
+final = ""
+for line in lines:
+    final += 'new Color("%s");\n' % line.strip()
+pyperclip.copy(final)
+    
